90fdf2550a4dcdf30e77a6bfd37859da9ea3d8f6
Шмяк <119859319+Shmak2281337@users.noreply.github.com>
2022-12-14 11:13:36 +0300

    Add files via upload

---
+++ zad4.pas
@@ -0,0 +1,12 @@
+﻿var s:string; l:integer;
+begin
+print('Введите строку: ');
+readln(s);
+l:=length(s);
+if l > 6 then
+begin
+  print(s[1],s[2],s[3],s[l],s[l-1],s[l-2])
+end
+else
+  println(s[1]*l)
+end.
